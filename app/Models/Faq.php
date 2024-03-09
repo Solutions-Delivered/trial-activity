dec9aa55ad0a604dce40c00585894eb913a527d8
Sam Jenkins <3852030+Satal@users.noreply.github.com>
2024-03-09 18:35:40 +0000

    Added models, factories, seeders and migration

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -5,9 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Service extends Model
+class Faq extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['name', 'slug', 'description'];
+    protected $fillable = ['question', 'answer'];
 }
--- app/Models/TeamMember.php
+++ app/Models/TeamMember.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Service extends Model
+class TeamMember extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug', 'description'];
+    protected $fillable = ['name', 'role', 'twitter', 'github', 'linkedin', 'image'];
 }
